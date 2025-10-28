228a7cc27872b5dfb3c65d550442608c69dca98d
Dan-Zorin <danzorinnewhorizon@gmail.com>
2025-10-28 00:15:03 -0500

    Nvidia Fix and Waydroid feature added for app deplayment

--- Host/nixos/Modules/KdeApp.nix
+++ Host/nixos/Modules/KdeApp.nix
@@ -1,17 +1,17 @@
+{ config, pkgs, ... }:
 {
   programs.kdeconnect = {
     enable = true;
     package = pkgs.kdePackages.kdeconnect-kde;
   };
 
-  # Optional: enable network discovery support (needed for pairing)
   services.avahi = {
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
   };
 
-  # Open KDE Connect ports in the firewall
   networking.firewall.allowedTCPPorts = [ 1714 1764 ];
   networking.firewall.allowedUDPPorts = [ 1714 1764 ];
 }
+
--- Host/nixos/Modules/Nvidia.nix
+++ Host/nixos/Modules/Nvidia.nix
@@ -1,43 +1,26 @@
-{ config, pkgs, unstable, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  #Nvidia hardware manageger 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
+    powerManagement.enable = true;
     open = false;
-    nvidiaSettings = true;	 
-    package = config.boot.kernelPackages.nvidiaPackages.production;   
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = with pkgs; [
-        vaapiVdpau
-        libvdpau-va-gl
-     ];
-   };
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 
-  # Enable the NVIDIA Container Toolkit for CDI support
-  hardware.nvidia-container-toolkit.enable = true;
-  
-  virtualisation.docker.extraOptions = ''
-  --add-runtime=nvidia=/run/current-system/sw/bin/nvidia-container-runtime
-  --default-runtime=nvidia
-  '';
-
-  #Nvidia Hardware environment variables
-  environment.sessionVariables = {
-        LIBVA_DRIVER_NAME = "nvidia";
-        VDPAU_DRIVER = "nvidia";
-        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        MOZ_ENABLE_WAYLAND = "1";
-        NVD_BACKEND = "direct";    # For Vulkan
-
-    };
-
-  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
-
+  boot.kernelParams = [
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nvidia.NVreg_EnablePCIeGen3=1"
+  ];
 }
+
